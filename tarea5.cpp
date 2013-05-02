a8c24037b471a6720c0befe004101d0c8c295105
Mario Ibarra <mariolp13@gmail.com>
2013-05-02 01:12:37 -0400

    actualizacion 1

--- tarea5.cpp
+++ tarea5.cpp
@@ -8,6 +8,16 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
+    if(strcmp(argv[1], "-s")==0)
+    { 
+         /*
+         select asignatura, avg(nota), STDDEV(nota) 
+         from asignaturas_cursadas
+         inner join cursos on cursos.curso_id = asignaturas_cursadas.curso_id
+         group by asignatura
+         */
+    }
+    
     if(strcmp(argv[1], "-v")==0)
     {
         char buffer[SIZE];
@@ -23,11 +33,15 @@ int main(int argc, char *argv[])
        /* Print out the date and time in the standard format. */
        fputs (asctime (loctime), stdout);
        cout<<endl;
+       cout<<"Version del software: "<<endl<<"Beta 1.5"<<endl<<endl;
        cout <<"Integrantes: "<<endl;       
        cout <<"Loreta Astudillo Ollig"<<endl; 
        cout <<"Mario Ibarra Tapia"<<endl;
        cout <<"Abraham Munoz Saez"<<endl;           
     }
+    
+   
+    
     
     system("PAUSE");
     return EXIT_SUCCESS;
