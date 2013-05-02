435c81174e9cde77db59945f874613a42a67100a
Mario Ibarra <mariolp13@gmail.com>
2013-05-02 00:02:50 -0400

    Integrando funciones de reconocimiento de parametros y opciones

--- tarea5.cpp
+++ tarea5.cpp
@@ -1,6 +1,8 @@
 #include <cstdlib>
 #include <iostream>
 #include <string.h>
+#include <time.h>
+#define SIZE 256
 
 using namespace std;
 
@@ -8,7 +10,23 @@ int main(int argc, char *argv[])
 {
     if(strcmp(argv[1], "-v")==0)
     {
-        cout <<"Hola mundo"<<endl;                   
+        char buffer[SIZE];
+       time_t curtime;
+       struct tm *loctime;
+     
+       /* Get the current time. */
+       curtime = time (NULL);
+     
+       /* Convert it to local time representation. */
+       loctime = localtime (&curtime);
+       cout <<"Hora del sistema: "<<endl<<endl;
+       /* Print out the date and time in the standard format. */
+       fputs (asctime (loctime), stdout);
+       cout<<endl;
+       cout <<"Integrantes: "<<endl;       
+       cout <<"Loreta Astudillo Ollig"<<endl; 
+       cout <<"Mario Ibarra Tapia"<<endl;
+       cout <<"Abraham Munoz Saez"<<endl;           
     }
     
     system("PAUSE");
